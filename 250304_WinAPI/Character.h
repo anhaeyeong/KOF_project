c8539ca9d8f9ac0595cb2b97aebc3f1dee3a5758
loy98 <yesfine13@gmail.com>
2025-03-19 17:06:27 +0900

    attack 추가

--- 250304_WinAPI/Character.h
+++ 250304_WinAPI/Character.h
@@ -10,6 +10,15 @@ enum class State
 	ATTACKED
 };
 
+typedef enum AttackedType
+{
+	NONE,
+	BIG_KICK,
+	SMALL_KICK,
+	BIG_PUNCH,
+	SMALL_PUNCH
+}at;
+
 class Image;
 class Character
 {
@@ -24,12 +33,19 @@ public:
 
 	void Move(int dx, int dy);
 	FPOINT GetPos() { return pos; }
+
+	void BigKick();
+	void SmallKick();
+	void BigPunch();
+	void SmallPunch();
+
 	
 	void SetPos(FPOINT pos) { pos = pos; }
 	void SetIsFlip(bool flip) { isFlip = flip; }
 	void SetFrame(int frame) { animationFrame = frame; }
 
 	int GetAnimationFrame() { return animationFrame; }
+	int GetFrame() { return frameCount; }
 	State GetState() { return _state; }
 
 private:
@@ -38,16 +54,26 @@ private:
 	int height;
 	int speed;
 	RECT attackRC;
+	bool attackRCactivated{ false };
 	RECT characterRC;
+
+	at attackType{ NONE };
 
 	Image* characterImage;
 	Image* punchImage;
 	Image* bigKickImage;
 	int animationFrame;
 
+	int frameCount{ 0 };
+
 	bool debugRender = true;
 	bool canMove;
 	bool isFlip;
 	State _state;
+
+
+	const float bigAttDamage{ 20 };
+	const float smallAttDamage{ 10 };
+	float nowAttDamage{ 0 };
 };
 
