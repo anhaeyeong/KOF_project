acfbb3e7fa76efbc9541dc1a613878c7b83be77a
anhaeyeong <91206465+anhaeyeong@users.noreply.github.com>
2025-03-20 23:10:59 +0900

    ClarkValidMoveUpdate

--- 250304_WinAPI/Clark.cpp
+++ 250304_WinAPI/Clark.cpp
@@ -173,7 +173,10 @@ void Clark::Move(int dir)
 	_state = State::MOVE;
 	if (canMove == false) return;
 	if (animationFrame >= 7)	animationFrame = 0;
-	pos.x += dir * speed;
+	if (CollisionManager::GetInstance()->isValidMove(this))
+	{
+		pos.x += dir * speed;
+	}
 	SetRectAtCenter(characterRC, pos.x, pos.y, width, height);
 	//pos.y += dy;
 	animationFrame++;
