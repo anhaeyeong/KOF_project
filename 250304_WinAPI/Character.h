0ee5f0fa7bb93f640dd892942b2223958e601b28
loy98 <yesfine13@gmail.com>
2025-03-21 11:35:48 +0900

    fix

--- 250304_WinAPI/Character.h
+++ 250304_WinAPI/Character.h
@@ -115,7 +115,7 @@ protected:
 	vector<Image*> animImages;
 	// animImages[AnimationType::IDLE] <- 이런 식으로 접근해서 사용
 	int animationFrame;
-	int maxIdlePrame;
+	int maxIdleFrame;
 	int modifyingValue{ 0 };
 	int maxAttackedFrame;
 
